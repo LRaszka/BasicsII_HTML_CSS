2c087941c0a2927e0806615df1badeb9638f7849
LRaszka <Raszka.Lukas123@gmail.com>
2021-08-14 18:57:49 +0200

    Menu scroll

--- script.js
+++ script.js
@@ -4,5 +4,22 @@
         $(".jq--scroll-our-food").click(function(){
             $("html, body").animate({scrollTop: $(".jq--our-food").offset().top}, 1000);
         });
+
+        $(".jq--scroll-about-food").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--about-food").offset().top}, 900);
+        });
+
+        $(".jq--scroll-reference").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--reference").offset().top}, 900);
+        });
+
+        $(".jq--scroll-photo").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--photo").offset().top}, 900);
+        });
+
+        $(".jq--scroll-form").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--form").offset().top}, 900);
+        });
+
     });
 })(jQuery);
